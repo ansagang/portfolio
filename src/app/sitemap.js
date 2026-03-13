9d9e6a7908087302173054b0449ef04397262aba
ansagang <ansagaang@gmail.com>
2026-03-13 13:38:49 +0100

    sitemap change

--- src/app/sitemap.js
+++ src/app/sitemap.js
@@ -9,14 +9,14 @@ export default async function sitemap() {
     const {data: projects} = await getProjects({lang: language.lang})
     const projectEntries = projects?.map((project) => ({
         url: `${BASE_URL}/projects/${project.slug}`,
-        lastModified: project.createdAt
+        lastModified: project.createdAt, changeFrequency: "daily", priority: 0.5
     })) ?? []
 
     return [
-        { url: `${BASE_URL}`, lastModified: new Date() },
-        { url: `${BASE_URL}/about`, lastModified: new Date() },
-        { url: `${BASE_URL}/projects`, lastModified: new Date() },
-        { url: `${BASE_URL}/contact`, lastModified: new Date() },
+        { url: `${BASE_URL}`, lastModified: new Date(), changeFrequency: "daily", priority: 1 },
+        { url: `${BASE_URL}/about`, lastModified: new Date(), changeFrequency: "daily", priority: 0.7 },
+        { url: `${BASE_URL}/projects`, lastModified: new Date(), changeFrequency: "daily", priority: 0.7 },
+        { url: `${BASE_URL}/contact`, lastModified: new Date(), changeFrequency: "daily", priority: 0.7 },
         ...projectEntries
     ]
 }
